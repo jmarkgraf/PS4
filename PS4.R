85d311b583d701fec93e575d1354a126358f37d8
jmarkgraf <jonas.markgraf@gmail.com>
2017-03-03 19:51:37 -0600

    Data Scraping and Cleaning
    Data Scraping successful.
    Data Cleaning almost complete.

--- PS4.R
+++ PS4.R
@@ -4,7 +4,26 @@ wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_ele
 ## Grab the tables from the page and use the html_table function to extract the tables.
 ## You need to subset temp to find the data you're interested in (HINT: html_table())
 
+# scaping data
 temp <- wikiURL %>% 
   read_html %>%
-  html_nodes("table")
+  html_nodes(xpath = '//*[@id="mw-content-text"]/table[2]') %>%
+  html_table()
+election <- temp[[1]]
 
+# clean data
+names(election)[1] <- "elecID"
+names(election)[2] <- "year"
+names(election)[3] <- "winner_candidate"
+names(election)[4] <- "winner_party"
+names(election)[5] <- "college_votes"
+names(election)[6] <- "college_voteshare"
+names(election)[7] <- "pop_voteshare"
+names(election)[8] <- "pop_marginperc"
+names(election)[9] <- "pop_votes"
+names(election)[10] <- "pop_marginabs"
+names(election)[11] <- "bestloser_candidate"
+names(election)[12] <- "bestloser_party"
+names(election)[13] <- "Turnout"
+
+test <- election[-(1:2), ]  # delete first 2 observations
