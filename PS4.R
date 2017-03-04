0337f1e23e94d839507b462cd82487419f43fa1b
jmarkgraf <jonas.markgraf@gmail.com>
2017-03-03 21:08:05 -0600

    Update: Visualization and Data Cleaning
    Data Cleaning almost complete: one variable cannot be solved due to
    unknown sign (looks like minus sign)
    Visualization: one trend plotted

--- PS4.R
+++ PS4.R
@@ -22,8 +22,37 @@ names(election)[7] <- "pop_voteshare"
 names(election)[8] <- "pop_marginperc"
 names(election)[9] <- "pop_votes"
 names(election)[10] <- "pop_marginabs"
-names(election)[11] <- "bestloser_candidate"
-names(election)[12] <- "bestloser_party"
-names(election)[13] <- "Turnout"
+names(election)[11] <- "loser_candidate"
+names(election)[12] <- "loser_party"
+names(election)[13] <- "turnout"
 
-test <- election[-(1:2), ]  # delete first 2 observations
+election <- election[-(1:2), ]  # delete first 2 observations
+
+election <- as.data.frame(sapply(election, gsub, pattern = "\\[a\\]", replacement = ""))  # remove footnote
+
+# turn variables into correct format
+election$year <- as.numeric(as.character(election$year))
+election$elecID <- as.numeric(as.character(election$elecID))
+election$winner_candidate <- as.character(election$winner_candidate)
+election$winner_party <- as.character(election$winner_party)
+
+election$college_voteshare <- as.numeric(as.character(gsub("%", "", election$college_voteshare)))
+election$pop_voteshare <- as.numeric(as.character(gsub("%", "", election$pop_voteshare)))
+election$pop_marginperc <- as.character(gsub("%", "", election$pop_marginperc))
+election$pop_marginperc <- as.numeric(gsub("[^0-9]", "-", election$pop_marginperc))# solve minus sign!!
+election$pop_votes <- as.numeric(as.character(gsub(",", "", election$pop_votes)))
+election$pop_marginabs <- as.character(gsub(",", "", election$pop_marginabs))
+election$pop_marginabs <- as.numeric(gsub("[^0-9]", "-", election$pop_marginabs))
+
+election$loser_candidate <- as.character(election$loser_candidate)
+election$loser_party <- as.character(election$loser_party)
+
+election$turnout <- as.numeric(as.character(gsub("%", "", election$turnout)))
+
+# VISUALIZATION -----------------------
+# ==============
+
+# college voteshare over time
+plot(election$elecID, election$college_voteshare, type = "p")
+points(election$elecID, election$pop_voteshare, pcl)
+
