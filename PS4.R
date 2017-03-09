e92820c4be96415f62fd10a31f129abfe5b0031b
jmarkgraf <jonas.markgraf@gmail.com>
2017-03-08 20:26:18 -0600

    Final Version: R Script.

--- PS4.R
+++ PS4.R
@@ -1,8 +1,22 @@
-library(rvest)    
-wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_elections_by_popular_vote_margin'
+# Problem Set 4: Webscraping; Data Cleaning; Plotting
+# Date: March 3, 2017
+# Author: Jonas Markgraf
+# ====================================================
 
-## Grab the tables from the page and use the html_table function to extract the tables.
-## You need to subset temp to find the data you're interested in (HINT: html_table())
+# load packages
+library(rvest) 
+library(repmis)
+
+# clean environment
+rm(list = ls())
+
+# set working directory
+# set working directory
+possibles <- c("~/Dropbox/Hertie School/(4) Applied Statistical Programming (WUSTL)/Repositories/PS4")
+set_valid_wd(possibles)
+
+# take the Wiki URL
+wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_elections_by_popular_vote_margin'
 
 # scaping data
 temp <- wikiURL %>% 
@@ -12,74 +26,53 @@ temp <- wikiURL %>%
 election <- temp[[1]]
 
 # clean data
-names(election)[1] <- "elecID"
-names(election)[2] <- "year"
-names(election)[3] <- "winner_candidate"
-names(election)[4] <- "winner_party"
-names(election)[5] <- "college_votes"
-names(election)[6] <- "college_voteshare"
-names(election)[7] <- "pop_voteshare"
-names(election)[8] <- "pop_marginperc"
-names(election)[9] <- "pop_votes"
-names(election)[10] <- "pop_marginabs"
-names(election)[11] <- "loser_candidate"
-names(election)[12] <- "loser_party"
-names(election)[13] <- "turnout"
+## new column names 
+names(election) <- c("elecID", "year", "winner_candidate", "winner_party",
+                     "college_votes", "college_voteshare", "pop_voteshare",
+                     "pop_marginperc", "pop_votes", "pop_marginabs", "loser_candidate",
+                     "loser_party", "turnout")
 
-election <- election[-(1:2), ]  # delete first 2 observations
+## first 2 observations do not contain meaningful information
+election <- election[-(1:2), ]  
 
-election <- as.data.frame(sapply(election, gsub, pattern = "\\[a\\]", replacement = ""))  # remove footnote
+## remove special character '[a]' (footnote) from data frame
+election <- as.data.frame(sapply(election, gsub, pattern = "\\[a\\]", replacement = "")) 
 
 # turn variables into correct format
-election$year <- as.numeric(as.character(election$year))
-election$elecID <- as.numeric(as.character(election$elecID))
-election$winner_candidate <- as.character(election$winner_candidate)
-election$winner_party <- as.character(election$winner_party)
+## remove special characters and turn factors in numeric
+ToNumeric <- function(var) {
+  # function removes special characters from variables and turns them into numeric variables
+  # remove special characters
+  new_var <- gsub('[^0-9\\.]', '', var)
+  # turn string into numeric
+  new_var2 <- as.numeric(as.character(new_var))
+  return(new_var2)
+}
 
-election$college_voteshare <- as.numeric(as.character(gsub("%", "", election$college_voteshare)))
-election$pop_voteshare <- as.numeric(as.character(gsub("%", "", election$pop_voteshare)))
+ToCharacter <- function(var) {
+  # function turns variables into numeric
+  # purpose: names and parties are treated as factors
+  new_var <- as.character(var)
+  return(new_var)
+}
+
+## apply function to adequate variables
+election[ , c(1:2, 6:7, 9, 13)] <- apply(election[ , c(1:2, 6:7, 9, 13)], 2, ToNumeric)
+election[ , c(3:4, 11:12)] <- apply(election[ , c(3:4, 11:12)], 2, ToCharacter)
+
+# deal with special cases that contain not identified minus sign
+## remove unwanted characters
 election$pop_marginperc <- as.character(gsub("%", "", election$pop_marginperc))
-election$pop_marginperc <- as.numeric(gsub("[^0-9]", "-", election$pop_marginperc))# solve minus sign!!
-election$pop_votes <- as.numeric(as.character(gsub(",", "", election$pop_votes)))
 election$pop_marginabs <- as.character(gsub(",", "", election$pop_marginabs))
-election$pop_marginabs <- as.numeric(gsub("[^0-9]", "-", election$pop_marginabs))
 
-election$loser_candidate <- as.character(election$loser_candidate)
-election$loser_party <- as.character(election$loser_party)
-
-election$turnout <- as.numeric(as.character(gsub("%", "", election$turnout)))
+## deal with minus sign
+# election$pop_marginperc <- as.numeric(gsub("[^0-9]", "-", election$pop_marginperc))  # no solution how to deal with minus here
+election$pop_marginabs <- as.numeric(gsub("[^0-9]", "-", election$pop_marginabs))  # this works
 
 # VISUALIZATION -----------------------
 # ==============
 
-# college vs. popular voteshare over time
-plot(election$year, election$college_voteshare, type = "p", ylim = c(25, 110),
-     main = "College Election Vs. Popular Voteshare, 1824-2016",
-     ylab = "Voteshare (%)", xlab = "Election Year")
-points(election$year, election$pop_voteshare, pch = 3, col = "red")
-legend("topleft", c("College", "Popular"), pch = c(1, 3), col = c("black", "red"), cex = 0.6)
-
-# Comment: in all elections was the electoral college voteshare of the elected president higher than the popular voteshare
-
-# discrepancy between college and popular voteshare by party
-with(election[election$winner_party == "Rep.", ], plot(year, college_voteshare - pop_voteshare,
-     main = "Discrepancy College and Popular Voteshare \nby Winning Party, 1824-2016",
-     ylab = "Difference (in %)", xlab = "Election Year", pch = 2, col = "red",
-     ylim = c(0, 60)))
-with(election[election$winner_party == "Rep.", ], 
-     abline(lm((college_voteshare - pop_voteshare) ~ year),
-     col = "red"))
-with(election[election$winner_party == "Dem.", ], points(year, college_voteshare - pop_voteshare,
-     pch = 0, col = "blue"))
-with(election[election$winner_party == "Dem.", ], 
-     abline(lm((college_voteshare - pop_voteshare) ~ year),
-     col = "blue"))
-legend("topleft", c("Republican", "Democrat"), pch = c(2, 0), col = c("red", "blue"), cex = 0.6)
-
-## Comment: historically, no party benefitted more from discrepancy; the last 5 elections had low discrepancies.
-
-# exporting graphs in pdf
-setwd("/Users/jonasmarkgraf/Dropbox/Hertie School/(4) Applied Statistical Programming (WUSTL)/Repositories/PS4")
+# PLOT 1: college vs. popular voteshare over time
 pdf(file = "ElectionPlots.pdf")
 par(mfrow(c(1,2)))
 # college vs. popular voteshare over time
@@ -89,7 +82,11 @@ plot(election$year, election$college_voteshare, type = "p", ylim = c(25, 110),
 points(election$year, election$pop_voteshare, pch = 3, col = "red")
 legend("topleft", c("College", "Popular"), pch = c(1, 3), col = c("black", "red"), cex = 0.6)
 
-# discrepancy between college and popular voteshare by party
+# Comment: historically, in all elections was the electoral college voteshare of the elected president higher 
+# than his popular voteshare
+
+
+# PLOT 2: discrepancy between college and popular voteshare by party
 with(election[election$winner_party == "Rep.", ], plot(year, college_voteshare - pop_voteshare,
                                                        main = "Discrepancy College and Popular Voteshare \nby Winning Party, 1824-2016",
                                                        ylab = "Difference (in %)", xlab = "Election Year", pch = 2, col = "red",
@@ -105,3 +102,51 @@ with(election[election$winner_party == "Dem.", ],
 legend("topleft", c("Republican", "Democrat"), pch = c(2, 0), col = c("red", "blue"), cex = 0.6)
 dev.off()
 
+## Comment: historically, no party benefitted more from discrepancy; 
+## the last 5 elections had low discrepancies between college vote and popular vote
+
+# Scraping electoral college votes by candidate ----------------------------------
+# ============================================
+
+# take the new URL
+wiki2URL <- 'https://en.wikipedia.org/wiki/United_States_presidential_election'
+
+# scaping data
+temp2 <- wiki2URL %>%   # this does not work because columns have different lengths ("table has inconsistent number of columns")
+  read_html %>%
+  html_nodes(xpath='//*[@id="mw-content-text"]/table[2]') %>%
+  html_table()
+
+# new package required
+library(htmltab)
+
+# scraping data
+college <- as.data.frame(htmltab(wiki2URL, which = 3))
+
+# extract new dataframe with electoral votes for winner and runner up
+
+
+# clean data
+
+## turn "Electoral Votes" into numeric and delete not wanted information after "/"
+college$electoral <- gsub("/.*","", college[,"Electoral votes"])  # delete last part
+college$electoral <- as.numeric(gsub("^\\s+|\\s+$", "", college$electoral))  # trim whitespace
+
+## set up new data frame to export electoral votes for winner and runner up
+college.export <- as.data.frame(unique(college$Year))
+names(college.export)[1] <- "year"
+college.export$year <- ToNumeric(college.export$year)
+
+## identify electoral votes for winner 
+college.export$college_winner <- tapply(college$electoral, INDEX = college$Year, FUN = max)
+
+## identify electoral votes for runner up 
+### write function to find second highest value
+college.export$college_runnerup <- tapply(college$electoral, INDEX = college$Year, 
+                                     FUN = function(i) sort(i, decreasing = TRUE)[2])
+
+# merge data frames
+full.elections <- merge(election, college.export, by = "year")
+
+# now save data file
+save(full.elections, file='US_Elections.Rdata')
